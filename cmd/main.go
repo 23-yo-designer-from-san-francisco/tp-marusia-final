8fa42aa2b0c6c120f7a701ec0351ae80cdba3862
Your Name <you@example.com>
2022-10-29 15:32:03 +0300

    gspd

--- cmd/main.go
+++ cmd/main.go
@@ -5,8 +5,6 @@ import (
 	"net/http"
 )
 
-import "encoding/json"
-
 type myPayload struct {
 	Text string
 	marusia.DefaultPayload
@@ -24,17 +22,6 @@ func main() {
 			case marusia.OnStart:
 				resp.Text = "Скилл запущен"
 				resp.TTS = "Скилл запущен, жду команд"
-			case "картинка":
-				resp.Card = marusia.NewBigImage(
-					"Заголовок",
-					"Описание",
-					457239017,
-				)
-			case "картинки":
-				resp.Card = marusia.NewImageList(
-					457239017,
-					457239018,
-				)
 			case "кнопки":
 				resp.Text = "Держи кнопки"
 				resp.TTS = "Жми на кнопки"
@@ -43,59 +30,16 @@ func main() {
 				resp.AddButton("подсказка с нагрузкой", myPayload{
 					Text: "test",
 				})
-			case "ссылка":
-				resp.Text = marusia.CreateDeepLink(
-					"e7a7d540-3928-4f11-87bf-a0de1244c096",
-					map[string]string{"Text": "нагрузка из ссылки"},
-				)
-				resp.TTS = "Держи диплинк"
-			case "пуш":
-				resp.Text = `Держи пуш`
-				resp.TTS = `Отправила пуш на устройство`
-				resp.Push.PushText = "Hello, i am push"
 			case "музон":
-				player := marusia.AudioPlayer{
-					Playlist: []marusia.AudioPlaylist{
-						{
-							Stream: marusia.AudioStream{
-								Source:     "2000512001_456239024",
-								SourceType: "vk",
-								TrackID:    "gspd-u-rossii-tri-puti",
-							},
-						},
-					},
-				}
-				resp.AudioPlayer = &player
+				resp.Text = "Что сейчас играет? <speaker audio_vk_id=2000512001_456239024>"
 			case marusia.OnInterrupt:
 				resp.Text = "Скилл закрыт"
 				resp.TTS = "Пока"
 				resp.EndSession = true
 			default:
 				resp.Text = "Неизвестная команда"
-				resp.TTS = "Я вас не поняла"
+				resp.TTS = "Я вас \"НЕ СОВСЕМ\" поняла"
 			}
-		case marusia.ButtonPressed:
-			var p myPayload
-
-			err := json.Unmarshal(r.Request.Payload, &p)
-			if err != nil {
-				resp.Text = "Что-то пошло не так"
-				return
-			}
-
-			resp.Text = "Кнопка нажата. Полезная нагрузка: " + p.Text
-			resp.TTS = "Вы нажали на кнопку"
-		case marusia.DeepLink:
-			var p myPayload
-
-			err := json.Unmarshal(r.Request.Payload, &p)
-			if err != nil {
-				resp.Text = "Что-то пошло не так"
-				return
-			}
-
-			resp.Text = "Специальная ссылка. Полезная нагрузка: " + p.Text
-			resp.TTS = "Вы перешли по ссылке"
 		}
 
 		return
