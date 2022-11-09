566f955ca01083eafceef091ccc2a711fd042b11
Your Name <you@example.com>
2022-11-10 01:45:53 +0300

    fucking garbage

--- microservice/music/repository.go
+++ microservice/music/repository.go
@@ -3,6 +3,6 @@ package music
 import "guessTheSongMarusia/models"
 
 type Repository interface {
-	CreateTrack(*models.VKTrack) (error)
+	CreateTrack(*models.VKTrack) error
 	GetSongsByArtists(artist string) ([]models.VKTrack, error)
 }
--- microservice/music/repository/repository.go
+++ microservice/music/repository/repository.go
@@ -1,17 +1,20 @@
 package repository
 
 import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"github.com/sirupsen/logrus"
+
 	//log "autfinal/pkg/logger"
 	"guessTheSongMarusia/models"
 	log "guessTheSongMarusia/pkg/logger"
-	"github.com/jmoiron/sqlx"
 )
 
 const (
 	insertTrackQuery = `insert into music (title, artist, duration_two_url, duration_three_url, duration_five_url, duration_fifteen_url, human_title)
 		values ($1, $2, $3, $4, $5, $6, $7) returning id;`
-	insertArtistQuery = `insert into artist (music_id, artist, human_artist) values ($1, $2, $3);`
-	getSongsByHumanArtist =  `select m.title, m.artist, m.duration_two_url, m.duration_three_url, m.duration_five_url, m.duration_fifteen_url, m.human_title 
+	insertArtistQuery     = `insert into artist (music_id, artist, human_artist) values ($1, $2, $3);`
+	getSongsByHumanArtist = `select m.title, m.artist, m.duration_two_url, m.duration_three_url, m.duration_five_url, m.duration_fifteen_url, m.human_title 
 		from music as m join artist on artist.music_id = m.id where artist.human_artist = $1;
 	`
 )
@@ -26,20 +29,22 @@ func NewMusicRepository(db *sqlx.DB) *MusicRepository {
 	}
 }
 
-func(mR *MusicRepository) CreateTrack(track *models.VKTrack) (error) {
+func (mR *MusicRepository) CreateTrack(track *models.VKTrack) error {
 	tx, err := mR.db.Beginx()
 	if err != nil {
+		fmt.Println(err)
 		tx.Rollback()
 		return err
 	}
-	
-	var trackId int 
-	err = tx.QueryRowx(insertTrackQuery, 
+
+	var trackId int
+	err = tx.QueryRowx(insertTrackQuery,
 		&track.Title, &track.Artist,
-		&track.Duration2, &track.Duration3, &track.Duration5, &track.Duration15, 
+		&track.Duration2, &track.Duration3, &track.Duration5, &track.Duration15,
 		&track.HumanTitle).Scan(&trackId)
 
 	if err != nil {
+		logrus.Error(err)
 		tx.Rollback()
 		log.Error(err.Error())
 		return err
@@ -48,7 +53,7 @@ func(mR *MusicRepository) CreateTrack(track *models.VKTrack) (error) {
 	for index, artist := range track.HumanArtists {
 		_, err = tx.Exec(insertArtistQuery, &trackId, &track.Artists[index], &artist)
 		if err != nil {
-			log.Error(err.Error())
+			logrus.Error(err.Error())
 			tx.Rollback()
 			return err
 		}
