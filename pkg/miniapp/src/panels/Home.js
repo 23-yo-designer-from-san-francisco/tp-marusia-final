8e84bd7319105b3c7fb92f6781e0470439ed2216
Leonid Belyaev <l.belyaev@vk.team>
2022-11-23 18:29:50 +0300

    added snackbar

--- pkg/miniapp/src/panels/Home.js
+++ pkg/miniapp/src/panels/Home.js
@@ -1,16 +1,18 @@
 import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 
-import {Button, Group, Header, Panel, PanelHeader, SizeType} from '@vkontakte/vkui';
+import {Avatar, Button, Group, Header, Panel, PanelHeader, SizeType, Snackbar} from '@vkontakte/vkui';
 import {Tracks} from "../components/Tracks";
 import {PlaylistTitle} from "../components/PlaylistTitle";
 import {BASE_URL} from "../constants";
+import {Icon16Done} from "@vkontakte/icons";
 
 const Home = ({id, showLoader}) => {
     const [tracks, setTracks] = useState();
     const [playlistTitle, setPlaylistTitle] = useState('');
     const [selectedTracks, setSelectedTracks] = useState(null);
     const [titleKey, setTitleKey] = useState('');
+    const [snackbar, setSnackbar] = useState(null);
 
     async function fetchData() {
         showLoader(true);
@@ -36,6 +38,22 @@ const Home = ({id, showLoader}) => {
                 music_ids: tr,
             })
         });
+        setSnackbar(
+            <Snackbar
+                mode="dark"
+                onClose={() => setSnackbar(null)}
+                before={
+                    <Avatar
+                        size={24}
+                        style={{background: "var(--vkui--color_background_accent)"}}
+                    >
+                        <Icon16Done fill="#fff" width={14} height={14}/>
+                    </Avatar>
+                }
+            >
+                Плейлист сохранен
+            </Snackbar>
+        );
     }
 
     useEffect(() => {
@@ -52,6 +70,7 @@ const Home = ({id, showLoader}) => {
             <Group header={<Header mode="secondary">Доступные треки</Header>}>
                 <Tracks updateSelectedTracks={setSelectedTracks} tracks={tracks}/>
             </Group>
+            {snackbar}
         </Panel>
     );
 };
